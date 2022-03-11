d72b1a49bf4ceb957a0ad91cb8d58e81b7921c32
damadam541 <damadam541@gmail.com>
2022-03-11 14:18:11 -0500

    Commit from eclipse

--- src/Testing.java
+++ src/Testing.java
@@ -5,6 +5,8 @@ public class Testing {
 		// TODO Auto-generated method stub
 
 		System.out.println("git pepeg");
+		System.out.println("Pushing from eclipse works!");
+		// TODO Find how to push changes to GitHub
 		// Hello from Eclipse -Adam
 	}
 
