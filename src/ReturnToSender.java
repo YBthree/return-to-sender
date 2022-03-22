5b5e30209358c132d4bdbdff65087f24f80b5389
damadam541 <damadam541@gmail.com>
2022-03-22 09:49:40 -0400

    Created required methods (Player, Officer, Island, Main)

---
+++ src/ReturnToSender.java
@@ -0,0 +1,10 @@
+
+public class ReturnToSender {
+
+	public static void main(String[] args) {
+		//This is main gameplay class
+		//Main class to be connected to Player, Island, Officer (NPC) classes
+		
+	}
+
+}
